3f676e444c8fa096146b1a45cc579c8efd148ee2
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-03-02 17:14:22 -0800

    did CSS

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -2,4 +2,8 @@ $(function() {
 
   $(".feedposts img").css("height", $(".feedposts img").width());
 
+  $(window).on("resize", function() {
+    $(".feedposts img").css("height", $(".feedposts img").width());
+  });
+
 });
