7af306f1aae852f4d84de1427878bcf4194380e0
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-03-05 16:15:53 -0800

    got rid of jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-1.7.1.js
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
